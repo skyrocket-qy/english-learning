1c807de03615c5b5e30b35680d20e8b5823aaaca
jimmy <rivendinner@gmail.com>
2025-08-10 03:17:32 +0800

    backup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,28 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
 
-export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+// export default {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//       },
+//       keyframes: {
+//         shake: {
+//           '0%, 100%': { transform: 'translateX(0)' },
+//           '25%': { transform: 'translateX(-4px)' },
+//           '75%': { transform: 'translateX(4px)' },
+//         },
+//       },
+//       animation: {
+//         shake: 'shake 0.3s ease-in-out',
+//       },
+//     },
+//   },
+//   plugins: [],
+// } satisfies Config;
